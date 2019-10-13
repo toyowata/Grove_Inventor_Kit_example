ac37438d93b17b37dab374fb2c05bf3c4a017de9
Toyomasa Watarai <toyomasa.watarai@gmail.com>
2019-10-13 14:34:51 +0900

    Add example 3

---
+++ 3_LED_Bar_Control.py
@@ -0,0 +1,26 @@
+from microbit import *
+
+def create_bar(value, max_val):
+    img = ""
+    for i in range(5):
+        str = "00000:"
+        leftovers = (max_val/5)
+        if (value > leftovers*(5-i)):
+            str = "99999:"
+        elif (value > (leftovers*(4-i) + (leftovers/6)*5)):
+            str = "59995:"
+        elif (value > (leftovers*(4-i) + (leftovers/6)*4)):
+            str = "09990:"
+        elif (value > (leftovers*(4-i) + (leftovers/6)*3)):
+            str = "05950:"
+        elif (value > (leftovers*(4-i) + (leftovers/6)*2)):
+            str = "00900:"
+        elif (value > (leftovers*(4-i) + (leftovers/6)*1)):
+            str = "00500:"
+        img += str
+    return Image(img)
+
+while True:
+    ain = pin0.read_analog()
+    display.show(create_bar(ain, 1023))
+    sleep(100)
